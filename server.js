a52528ea7734a2a89b3c4953d653766f71090767
Mike Barber <bandofgold1@gmail.com>
2023-09-30 12:49:28 -0400

    get routes
    set up get routes

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const fs = require('fs');
 
 // start express instance
 const app = express();
-
 // set port to 3001
 const PORT = 3001;
 
+//get route for HTML
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/notes.html'))
+});
+
+//get route for JS
+app.get('/assets/js/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public assets/js/index.js'));
+});
+
 //listen for incoming connections
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
